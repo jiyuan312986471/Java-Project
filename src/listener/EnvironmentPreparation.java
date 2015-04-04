d2ba60f99fee0c5ac03497ad04ae06f750f4006b
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-04-04 13:11:20 +0200

    init

---
+++ src/listener/EnvironmentPreparation.java
@@ -0,0 +1,15 @@
+package listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+public class EnvironmentPreparation implements ServletContextListener {
+
+	public void contextDestroyed(ServletContextEvent arg0) {
+	}
+
+	public void contextInitialized(ServletContextEvent evt) {
+	}
+
+}
