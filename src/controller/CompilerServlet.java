4aa21fca649cabe3d9753a2750085aa2401c582c
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-04-20 22:21:27 +0200

    .

--- src/controller/CompilerServlet.java
+++ src/controller/CompilerServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.Exercise;
+import model.User;
 
 @WebServlet("/CompilerServlet")
 public class CompilerServlet extends HttpServlet {
@@ -24,12 +26,19 @@ public class CompilerServlet extends HttpServlet {
 	
 	// Servlet Service
 	public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		// get exo and code
+		// get user, exo and code
+		User u = (User) req.getSession().getAttribute("User");
 		Exercise exo = (Exercise) req.getSession().getAttribute("exo");
 		String code = req.getParameter("code");
 		
 		// compile
-		DynamicCompile.compile(exo, code);
+		try {
+			DynamicCompile.compile(exo, code, u);
+		} catch (ClassNotFoundException | NoSuchMethodException
+				| SecurityException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
